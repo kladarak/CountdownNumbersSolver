151a937578a36d10d41cb745946d85fa4c4c2868
Scott Jenkins <scottjenkins88@gmail.com>
2015-09-15 22:13:01 +0100

    Optimised solution.

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1,6 +1,8 @@
 #include "Solver.h"
 #include "Operator.h"
 
+#include <algorithm>
+
 static const Operator sOperators[] =
 {
 	{ 
@@ -9,7 +11,7 @@ static const Operator sOperators[] =
 		"+" 
 	},
 	{ 
-		(Operator::OperandCheck)	[] (int inLHS, int inRHS)	{ return inLHS > inRHS; }, 
+		(Operator::OperandCheck)	[] (int inLHS, int inRHS)	{ return (inLHS - inRHS) > 0; }, 
 		(Operator::Operation)		[] (int inLHS, int inRHS)	{ return inLHS - inRHS; }, 
 		"-" 
 	},
@@ -34,53 +36,64 @@ Solver::Solver(const std::vector<int>& inNumbers, int inTarget)
 	}
 }
 
-static bool solveRecursion(const std::vector<Element*>& inElements, int inTarget)
+static void mySort(Element** inStart, int inCount)
 {
-	for (auto lhs : inElements)
+	std::sort(inStart, inStart + inCount, [] (Element* inLHS, Element* inRHS) { return inLHS->Value() > inRHS->Value(); } );
+}
+
+static bool solveRecursion(Element** inStart, int inCount, int inTarget)
+{
+	if (inCount == 1)
 	{
-		for (auto rhs : inElements)
+		return false;
+	}
+
+	// Alloc once per entry, rather than inside loop which may potentially stack overflow.
+	Element** newElems = (Element**) alloca(sizeof(Element*) * (inCount - 1));
+
+	for (int lhsIx = 0; lhsIx < (inCount - 1); ++lhsIx)
+	{
+		auto lhs = inStart[lhsIx];
+
+		for (int rhsIx = (lhsIx + 1); rhsIx < inCount; ++rhsIx)
 		{
-			if (lhs == rhs)
-			{
-				continue;
-			}
+			auto rhs = inStart[rhsIx];
 
 			for (size_t i = 0; i < sizeof(sOperators)/sizeof(Operator); ++i)
 			{
 				const Operator* op = &sOperators[i];
 				
-				if (op->CanOperate(lhs->Value(), rhs->Value()))
+				if (!op->CanOperate(lhs->Value(), rhs->Value()))
 				{
-					Compound result(lhs, op, rhs);
-					//printf("%s\n", result.ToString().c_str());
+					continue;
+				}
 
-					if (result.Value() == inTarget)
-					{
-						printf("%s = %i\n", result.ToString().c_str(), inTarget);
-						return true;
-					}
-					else if (inElements.size() == 2)
-					{
-						// Don't recurse further. 
-						continue;
-					}
-					else
-					{
-						std::vector<Element*> newElems;
-						newElems.push_back(&result);
+				Compound result(lhs, op, rhs);
+				//printf("%s\n", result.ToString().c_str());
 
-						for (auto elem : inElements)
+				if (result.Value() == inTarget)
+				{
+					printf("%s = %i\n", result.ToString().c_str(), inTarget);
+					return true;
+				}
+				else
+				{
+					int count = 0;
+					newElems[count++] = &result;
+
+					for (int elemIx = 0; elemIx < inCount; ++elemIx)
+					{
+						if (elemIx != lhsIx && elemIx != rhsIx)
 						{
-							if (elem != lhs && elem != rhs)
-							{
-								newElems.push_back(elem);
-							}
+							newElems[count++] = inStart[elemIx];
 						}
+					}
+
+					mySort(newElems, count);
 						
-						if (solveRecursion(newElems, inTarget))
-						{
-							return true;
-						}
+					if (solveRecursion(newElems, count, inTarget))
+					{
+						return true;
 					}
 				}
 			}
@@ -92,12 +105,15 @@ static bool solveRecursion(const std::vector<Element*>& inElements, int inTarget
 
 bool Solver::Solve()
 {
-	std::vector<Element*> elements;
+	Element** elems = (Element**) alloca(sizeof(Element*) * mNumbers.size());
+	int count = 0;
 	for (auto& num : mNumbers)
 	{
-		elements.push_back(&num);
+		elems[count++] = &num;
 	}
-	
-	return solveRecursion(elements, mTarget);
+
+	mySort(elems, count);
+
+	return solveRecursion(elems, count, mTarget);
 }
 
